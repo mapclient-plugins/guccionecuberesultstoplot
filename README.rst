fb9c57423424a423f5753a5e607020ca0c1c9260
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:13:12 +1200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,5 @@
+Guccione Cube Results to Plot
+=============================
+
+The Guccione Cube Results to Plot step is a plugin for the MAP Client application.
+
---
+++ mapclientplugins/guccionecuberesultstoplotstep/step.py
@@ -0,0 +1,144 @@
+
+"""
+MAP Client Plugin Step
+"""
+import os
+import json
+
+from PySide import QtGui
+
+from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
+from mapclientplugins.guccionecuberesultstoplotstep.configuredialog import ConfigureDialog
+
+
+class GuccioneCubeResultstoPlotStep(WorkflowStepMountPoint):
+    """
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    """
+
+    def __init__(self, location):
+        super(GuccioneCubeResultstoPlotStep, self).__init__('Guccione Cube Results to Plot', location)
+        self._configured = False # A step cannot be executed until it has been configured.
+        self._category = 'Utility'
+        # Add any other initialisation code here:
+        self._icon =  QtGui.QImage(':/guccionecuberesultstoplotstep/images/utility.png')
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#guccione_results_location'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#plot_datasets'))
+        # Port data:
+        self._portData0 = None # http://physiomeproject.org/workflow/1.0/rdf-schema#directory_location
+        self._portData1 = None # http://physiomeproject.org/workflow/1.0/rdf-schema#plot_datasets
+        # Config:
+        self._config = {}
+        self._config['identifier'] = ''
+
+
+    def execute(self):
+        """
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        """
+        # Put your execute step code here before calling the '_doneExecution' method.
+        # load data
+        results_filename = os.path.join(self._portData0, 'results.json')
+        with open(results_filename) as resultsFile:
+            results = json.load(resultsFile)
+
+        datasets = dict()
+        datasets['fibre_direction'] = {
+            'label': results['materialParameters'],
+            'x': results['fibre']['strain'],
+            'y': results['fibre']['stress'],
+            'marker': '-'
+        }
+        datasets['cross_direction'] = {
+            'x': results['cross']['strain'],
+            'y': results['cross']['stress'],
+            'marker': '1--'
+        }
+
+        self._portData1 = datasets
+
+        self._doneExecution()
+
+    def setPortData(self, index, dataIn):
+        """
+        Add your code here that will set the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        uses port for this step then the index can be ignored.
+
+        :param index: Index of the port to return.
+        :param dataIn: The data to set for the port at the given index.
+        """
+        self._portData0 = dataIn # http://physiomeproject.org/workflow/1.0/rdf-schema#directory_location
+
+    def getPortData(self, index):
+        """
+        Add your code here that will return the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        provides port for this step then the index can be ignored.
+
+        :param index: Index of the port to return.
+        """
+        return self._portData1 # http://physiomeproject.org/workflow/1.0/rdf-schema#plot_datasets
+
+    def configure(self):
+        """
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        """
+        dlg = ConfigureDialog(QtGui.QApplication.activeWindow().currentWidget())
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        """
+        The identifier is a string that must be unique within a workflow.
+        """
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        """
+        The framework will set the identifier for this step when it is loaded.
+        """
+        self._config['identifier'] = identifier
+
+    def serialize(self):
+        """
+        Add code to serialize this step to string.  This method should
+        implement the opposite of 'deserialize'.
+        """
+        return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def deserialize(self, string):
+        """
+        Add code to deserialize this step from string.  This method should
+        implement the opposite of 'serialize'.
+
+        :param string: JSON representation of the configuration in a string.
+        """
+        self._config.update(json.loads(string))
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
