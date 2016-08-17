fc8c64d7209657d09576b668116ac14c2081907b
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:20:42 +1200

    Set location of plugin.

--- mapclientplugins/guccionecuberesultstoplotstep/__init__.py
+++ mapclientplugins/guccionecuberesultstoplotstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Guccione Cube Results to Plot'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/guccionecuberesultstoplot/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.guccionecuberesultstoplotstep import step
